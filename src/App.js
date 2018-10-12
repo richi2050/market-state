d96dbeabeda995bcd6961283af7fdd498733f8fb
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-10-12 11:55:20 -0500

    importarcion de dependecias

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Badge } from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -18,8 +19,28 @@ class App extends Component {
             rel="noopener noreferrer"
           >
             Learn React
+
           </a>
         </header>
+
+        <div>
+          <Button color="primary">primary</Button>{' '}
+          <Button color="secondary">secondary</Button>{' '}
+          <Button color="success">success</Button>{' '}
+          <Button color="info">info</Button>{' '}
+          <Button color="warning">warning</Button>{' '}
+          <Button color="danger">danger</Button>{' '}
+          <Button color="link">link</Button>
+        </div>
+
+        <div>
+            <h1>Heading <Badge color="secondary">New</Badge></h1>
+            <h2>Heading <Badge color="secondary">New</Badge></h2>
+            <h3>Heading <Badge color="secondary">New</Badge></h3>
+            <h4>Heading <Badge color="secondary">New</Badge></h4>
+            <h5>Heading <Badge color="secondary">New</Badge></h5>
+            <h6>Heading <Badge color="secondary">New</Badge></h6>
+          </div>
       </div>
     );
   }

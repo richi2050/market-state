d02ba5fa64a3bed757593588207b202e7a615843
richi2050 <ricardo.lugo.recillas@gmail.com>
2018-10-12 12:40:55 -0500

    se implemento como componente el navegador

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button, Badge } from 'reactstrap';
+import Browser from './components/Nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-
-          </a>
-        </header>
-
-        <div>
-          <Button color="primary">primary</Button>{' '}
-          <Button color="secondary">secondary</Button>{' '}
-          <Button color="success">success</Button>{' '}
-          <Button color="info">info</Button>{' '}
-          <Button color="warning">warning</Button>{' '}
-          <Button color="danger">danger</Button>{' '}
-          <Button color="link">link</Button>
-        </div>
-
-        <div>
-            <h1>Heading <Badge color="secondary">New</Badge></h1>
-            <h2>Heading <Badge color="secondary">New</Badge></h2>
-            <h3>Heading <Badge color="secondary">New</Badge></h3>
-            <h4>Heading <Badge color="secondary">New</Badge></h4>
-            <h5>Heading <Badge color="secondary">New</Badge></h5>
-            <h6>Heading <Badge color="secondary">New</Badge></h6>
-          </div>
+        <Browser />
       </div>
     );
   }
